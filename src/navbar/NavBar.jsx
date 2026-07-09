be3564df216f5acadf08d174361b89cddcc62b6d
akashandy07 <akashandy07@gmail.com>
2026-07-09 07:54:03 -0700

    Added contact page

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,30 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import './NavBar.css'
 
 const NavBar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return (
         <div className="navbar">
             <div className="nav-bar-content">
                 <div className="navbar-inner">
-                    {/* Logo */}
+
                     <Link to="/">
                         <h1 className="logo-text">. aurion</h1>
                     </Link>
 
-                    {/* Nav Links */}
-                    <nav className="nav-links">
+                    {/* Hamburger */}
+                    <div
+                        className="menu-icon"
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        {menuOpen ? <FaTimes /> : <FaBars />}
+                    </div>
+
+                    {/* Navigation */}
+                    <nav className={menuOpen ? "nav-links active" : "nav-links"}>
                         <Link to="/about" className="nav-link">About</Link>
                         <Link to="/service" className="nav-link">Services</Link>
                         <Link to="/carrer" className="nav-link">Careers</Link>
                         <Link to="/insight" className="nav-link">Insights</Link>
                         <Link to="/contact" className="nav-link">Contact</Link>
+
+                        <div className="cta-group mobile-btn">
+                            <Link to="/login" className="client-login">
+                                Client Login
+                            </Link>
+
+                            <button className="talk-btn">
+                                Let's Talk
+                            </button>
+                        </div>
                     </nav>
 
-                    {/* CTA buttons */}
-                    <div className="cta-group">
-                        <Link to="/login" className="client-login">Client login</Link>
-                        <button className="talk-btn">Let's talk</button>
+                    <div className="cta-group desktop-btn">
+                        <Link to="/login" className="client-login">
+                            Client Login
+                        </Link>
+
+                        <button className="talk-btn">
+                            Let's Talk
+                        </button>
                     </div>
 
                 </div>
